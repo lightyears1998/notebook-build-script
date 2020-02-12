12fd7caea7fde15e7cb8b9651433695323a29f32
lightyears1998 <lightyears1998@hotmail.com>
2020-02-12 22:02:51 +0800

    Custom style for qfstudio.net

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ then
 else
     git clone https://github.com/lightyears1998/notebook.git notebook --depth=1 && cd notebook
 fi
+pipenv run python "../modify-conf.py"
+cp -r ../theme .
 pipenv run python -m mkdocs build -d "../out"
---
+++ modify-conf.py
@@ -0,0 +1,14 @@
+import yaml
+
+confpath = "./mkdocs.yml"
+with open(confpath, encoding="utf8") as f:
+    conf = f.read()
+conf = yaml.safe_load(conf)
+
+conf['site_name'] = '青色的麦芒'
+conf['site_url'] = 'https://qfstudio.net/'
+conf['site_description'] = '谢金宏的个人笔记本'
+conf['theme']['custom_dir'] = 'theme'
+
+with open(confpath, mode="w", encoding="utf8") as f:
+    f.write(yaml.safe_dump(conf, allow_unicode=True))
